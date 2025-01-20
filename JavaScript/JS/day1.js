1cb4d69490539e5a124306f0c31b221ebd43ff28
UdayBari3421 <udaybari2003@gmail.com>
2025-01-20 18:37:51 +0530

    JS1 done

---
+++ JavaScript/JS/day1.js
@@ -0,0 +1,64 @@
+// Console Information
+
+// console.log("Hello This is First Try");
+// console.error("This is Error");
+// console.warn("This Is Warning");
+// console.info("this is info");
+
+// var variable1;
+// let a = 10;
+
+// if (true) {
+//   var variable1 = 12;
+//   a = 14;
+//   console.log("In The Condition" + a);
+// }
+
+// console.log("Outer The Block" + a);
+
+//  if else
+let a = 10;
+let b = "uday";
+
+// if (a == 10) {
+//   console.log("The Value Is == 10");
+// }
+
+// if (a === "uday") {
+
+// }
+
+// ladder
+// if () { }
+// else if () { }
+// else if () { }
+// else if () { }
+// else{}
+
+// switch();
+
+// ternary
+// condition ? true : false;
+
+// 1 to 10;
+
+// for(let i = 1; i<=10 ; i++){}
+
+// while(i>10){
+// code
+// i++
+
+//  i am comment
+
+/*
+    i am
+    multiple
+    line 
+    comment
+*/
+
+// }
+
+// do {
+
+// } while ()
